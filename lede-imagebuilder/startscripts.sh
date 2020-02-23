616775be6de9010c91409a4fe559cce6fd67e180
12f23eddde <hrz6976@hotmail.com>
2020-02-23 19:48:04 +0800

    update startscripts.sh

--- lede-imagebuilder/startscripts.sh
+++ lede-imagebuilder/startscripts.sh
@@ -1,8 +1,9 @@
 if  [ -d "/lede/" ];then
-    sudo rm -rf /lede/*
+    sudo rm -rf /lede/
 fi
 git clone https://github.com/coolsnowwolf/lede /lede
 sudo chmod -R 777 /lede
 /lede/scripts/feeds update -a
 /lede/scripts/feeds install -a
 make menuconfig
+/bin/bash
